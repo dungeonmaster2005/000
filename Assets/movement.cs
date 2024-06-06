5563c1127d48f463d88cd1a87768d07098e124a2
Makima <arabadzhy@edu.cdv.pl>
2024-06-06 19:51:16 +0200

    333

---
+++ Assets/movement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(message"Hello from the start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(message"Hello from the start");  
+    }
+}
